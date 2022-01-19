512e0b454223ff48ff08e1cf7714c06f327f09c8
Nikhil Dung <dungnikhil18@gmail.com>
2022-01-19 09:32:12 -0800

    Adding Signup Pages

--- Patient/urls.py
+++ Patient/urls.py
@@ -6,8 +6,9 @@ urlpatterns =[
     path('Doctor',views.Doctor , name="Doctor"),
     path('Doctor_Precription', views.Doctor_Pre, name="Doc_Pre"),
     path('Doctor_Profile', views.Doctor_Profile, name="Doc_Pro"),
-    path('Doctor_App', views.Doctor_Appointment, name="Doc_App")
-
+    path('Doctor_App', views.Doctor_Appointment, name="Doc_App"),
+    path('Signup', views.signup,name="Sign_up"),
+    path('Login',views.LOGIN,name="Login")
 
 
 ]
--- Patient/views.py
+++ Patient/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate,login, logout
+from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+
 # Create your views here.
 def Home(request):
     return render(request,'Patient/Html/Index.html')
@@ -16,3 +21,47 @@ def Doctor_Pre(request):
 
 def Doctor_Appointment(request):
     return render(request, 'Patient/Html/Appointment.html')
+
+@csrf_exempt
+def signup(request):
+    ctx = {}
+    if request.POST:
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Home')
+        else:
+
+            ctx['form'] = form
+    else:
+        form = NewUserForm()
+        ctx['form'] = form
+    return render(request, 'Patient/Html/Signup.html', ctx)
+
+
+def LOGIN(request):
+
+    if request.POST:
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            pwd = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=pwd)
+            print(username,pwd,end='\n')
+            if user is not None:
+                login(request, user)
+                return redirect('Home')
+
+        messages.info(request, 'username and/or password not correct ')
+
+    form = AuthenticationForm()
+
+    return render(request, 'Patient/Html/Login.html', {
+        'login_form' : form
+    })
+
+
+def LOGOUT(request):
+    logout(request)
+    return redirect('Home')
